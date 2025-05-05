714d658faf4d7360ed0e5071a1545b473a115b7a
Muhammad Abubakar <abubakarsardar04@gmail.com>
2025-05-05 23:41:28 +0500

    update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -29,9 +29,8 @@ const Dashboard = () => {
         <div>
           <button 
             onClick={handleBackClick}
-            className="mb-4 flex items-center text-blue-600 hover:text-blue-800"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+            className="mb-4 flex items-center text-blue-600 hover:text-blue-800 cursor-pointer"  >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1  " viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
             </svg>
             Back to Accounts
